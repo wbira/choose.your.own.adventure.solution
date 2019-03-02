d853094f3db1303874ab7905702e718646c8056e
Waldemar Bira <biraw@ryanair.com>
2019-03-02 13:30:28 +0100

    Implemented navigation through stories chapters

--- pkg/handler.go
+++ pkg/handler.go
@@ -1,8 +1,10 @@
 package pkg
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
+	"strings"
 )
 
 func init() {
@@ -20,10 +22,24 @@ type handler struct {
 }
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	err := tpl.Execute(w, h.story["intro"])
-	if err != nil {
-		panic(err)
+	path := strings.TrimSpace(r.URL.Path)
+
+	if path == "" || path == "/" {
+		path = "/intro"
 	}
+
+	path = path[1:]
+
+
+	if chapter, ok := h.story[path]; ok {
+		err := tpl.Execute(w, chapter)
+		if err != nil {
+			fmt.Printf("%v", err)
+			http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		}
+		return
+	}
+	http.Error(w, "Status not found", http.StatusNotFound)
 }
 
 var defaultHandlerTemplate = `
